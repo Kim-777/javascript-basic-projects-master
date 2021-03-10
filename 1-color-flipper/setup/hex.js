d0c41a872d55b60208901aefe7b38e2255d9e32f
Kim-777 <iloo3012@naver.com>
2021-03-10 20:36:11 +0900

    finish course01 : color-flipper

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+
+    let hexColor = '#';
+
+    for(let i=0; i<6; i++) {
+
+        let temp = getRandomNumber()
+
+        //Math.random * hex.length 로 hex array character random select
+        hexColor += hex[temp];
+
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
